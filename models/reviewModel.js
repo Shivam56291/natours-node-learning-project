d15947aaa39bc06771c3e21fb1d1c4b902dcdd7e
Shivam Kumar <roysinghaniashivam@gmail.com>
2025-12-03 00:27:43 +0530

    fix: add return statements to prevent header errors in tour controller
    - Add explicit return after next() calls in getToursWithin and getDistances
    - Prevent "Cannot set headers after they are sent" errors
    - Update related route handlers and review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
   review: {
@@ -29,11 +30,52 @@ const reviewSchema = new mongoose.Schema({
   toObject: { virtuals: true },
 });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true }); 
+
 reviewSchema.pre(/^find/, function () {
   this.populate({
     path: 'user',
     select: 'name photo',
   });
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  this.r = await this.findOne();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
--- models/tourModel.js
+++ models/tourModel.js
@@ -35,6 +35,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, 'Rating must be above 1.0'],
       max: [5, 'Rating must be below 5.0'],
+      set: (val) => Math.round(val * 10) / 10,
     },
     ratingsQuantity: {
       type: Number,
@@ -115,6 +116,10 @@ const tourSchema = new mongoose.Schema(
   }
 );
 
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -166,11 +171,11 @@ tourSchema.pre('aggregate', function () {
   const firstStage = this.pipeline()[0];
 
   if (firstStage && firstStage.$match && firstStage.$match.secretTour) {
-    return next();
+    return;
   }
 
   if (firstStage && firstStage.$geoNear) {
-    return next();
+    return;
   }
 
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
